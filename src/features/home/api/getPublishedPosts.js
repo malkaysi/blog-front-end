bc74522ef7e961803475f3fa7c06bfced5f8940b
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2023-01-20 17:09:45 -0500

    Pulling published posts from backend api

--- src/features/home/api/getPublishedPosts.js
+++ src/features/home/api/getPublishedPosts.js
@@ -1,5 +1,13 @@
 
-export const getPublishedPosts = () => {
+export const getPublishedPosts = async () => {
   // Need to fetch the published posts
   // Let's just return the json for now
+  try {
+    const response = await fetch('http://localhost:3000/blog');
+    const publishedPostData = await response.json();
+
+    return publishedPostData;
+  } catch (err) {
+    console.log(err)
+  }
 }
--- src/features/home/index.js
+++ src/features/home/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getPublishedPosts } from './api/getPublishedPosts';
 
 const Home = () => {
 
-  // Show published comment cards
+  const [publishedPosts, setPublishedPosts] = useState({})
+
+  useEffect(() => {
+    (async () => {
+      const postResult = await getPublishedPosts();
+      setPublishedPosts(postResult);
+    })();
+  }, []);
 
   return (
-    <div>This will be the home page</div>
+    <>
+      <div>This will be the home page</div>
+      <div>publishedPosts: <pre>{JSON.stringify(publishedPosts)}</pre>
+      </div>
+    </>
   )
 }
 
